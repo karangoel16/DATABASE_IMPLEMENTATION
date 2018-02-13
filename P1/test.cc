dc9102677bb0a3e98dd1c4c724481b9acd6b3377
karangoel16 <karangoel16@yahoo.com>
2018-02-13 18:32:42 -0500

    files have been added A2

--- P1/test.cc
+++ P1/test.cc
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "DBFile.h"
 #include "test.h"
-
+#include "BigQ.h"
+#include <pthread.h>
 // make sure that the file path/dir information below is correct
-const char *dbfile_dir = ""; // dir where binary heap files should be stored
-const char *tpch_dir ="/home/karan/Downloads/GIT_HUB/tpch-dbgen/"; // dir where dbgen tpch files (extension *.tbl) can be found
-const char *catalog_path = "catalog"; // full path of the catalog file
+
 
 using namespace std;
 
@@ -74,9 +73,125 @@ void test3 () {
 	dbfile.Close ();
 }
 
+int add_data (FILE *src, int numrecs, int &res) {
+	DBFile dbfile;
+	dbfile.Open (rel->path ());
+	Record temp;
+
+	int proc = 0;
+	int xx = 20000;
+	while ((res = temp.SuckNextRecord (rel->schema (), src)) && ++proc < numrecs) {
+		dbfile.Add (temp);
+		if (proc == xx) cerr << "\t ";
+		if (proc % xx == 0) cerr << ".";
+	}
+
+	dbfile.Close ();
+	return proc;
+}
+
+
+// create a dbfile interactively
+void test4 () {
+
+
+	OrderMaker o;
+	rel->get_sort_order (o);
+
+	int runlen = 0;
+	while (runlen < 1) {
+		cout << "\t\n specify runlength:\n\t ";
+		cin >> runlen;
+	}
+	struct {OrderMaker *o; int l;} startup = {&o, runlen};
+
+	DBFile dbfile;
+	cout << "\n output to dbfile : " << rel->path () << endl;
+	dbfile.Create (rel->path(), sorted, &startup);
+	dbfile.Close ();
+
+	char tbl_path[100];
+	sprintf (tbl_path, "%s%s.tbl", tpch_dir, rel->name()); 
+	cout << " input from file : " << tbl_path << endl;
+
+        FILE *tblfile = fopen (tbl_path, "r");
+
+	srand48 (time (NULL));
+
+	int proc = 1, res = 1, tot = 0;
+	while (proc && res) {
+		int x = 0;
+		while (x < 1 || x > 3) {
+			cout << "\n select option for : " << rel->path () << endl;
+			cout << " \t 1. add a few (1 to 1k recs)\n";
+			cout << " \t 2. add a lot (1k to 1e+06 recs) \n";
+			cout << " \t 3. run some query \n \t ";
+			cin >> x;
+		}
+		if (x < 3) {
+			proc = add_data (tblfile,lrand48()%(int)pow(1e3,x)+(x-1)*1000, res);
+			tot += proc;
+			if (proc) 
+				cout << "\n\t added " << proc << " recs..so far " << tot << endl;
+		}
+		else {
+			test3 ();
+		}
+	}
+	cout << "\n create finished.. " << tot << " recs inserted\n";
+	fclose (tblfile);
+}
+
+// sequential scan of a DBfile 
+void test5 () {
+
+	cout << " scan : " << rel->path() << "\n";
+	DBFile dbfile;
+	dbfile.Open (rel->path());
+	dbfile.MoveFirst ();
+
+	Record temp;
+
+	int cnt = 0;
+	cerr << "\t";
+	while (dbfile.GetNext (temp) && ++cnt) {
+		temp.Print (rel->schema());
+		if (cnt % 10000) {
+			cerr << ".";
+		}
+	}
+	cout << "\n scanned " << cnt << " recs \n";
+	dbfile.Close ();
+}
+
+void test6 () {
+
+	CNF cnf; 
+	Record literal;
+	rel->get_cnf (cnf, literal);
+
+	DBFile dbfile;
+	dbfile.Open (rel->path());
+	dbfile.MoveFirst ();
+
+	Record temp;
+
+	int cnt = 0;
+	cerr << "\t";
+	while (dbfile.GetNext (temp, cnf, literal) && ++cnt) {
+		temp.Print (rel->schema());
+		if (cnt % 10000 == 0) {
+			cerr << ".";
+		}
+	}
+	cout << "\n query over " << rel->path () << " returned " << cnt << " recs\n";
+	dbfile.Close ();
+
+}
+
 int main () {
 
-	setup (catalog_path, dbfile_dir, tpch_dir);
+	setup ();
 
 	void (*test) ();
 	relation *rel_ptr[] = {n, r, c, p, ps, o, li};
