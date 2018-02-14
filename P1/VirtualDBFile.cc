a719b7f85e6c39b3230d646cf2a64d4e0b47749d
karangoel16 <karangoel16@yahoo.com>
2018-02-14 04:42:05 -0500

    Sorted class can now work

---
+++ P1/VirtualDBFile.cc
@@ -0,0 +1,51 @@
+#include "VirtualDBFile.h"
+
+void VirtualDBFile::MoveFirst(){
+    check_write();
+    readPage.EmptyItOut(); // to remove anything in the array
+    file.GetPage(&readPage,currentPage=0);
+}
+
+int VirtualDBFile::Close(){
+    check_write();
+    file.Close();
+    return 1;
+}
+
+int VirtualDBFile::GetNext(Record &fetchme){
+    check_write(); //to check if we need to write before getNext in case some records have been written in the file
+    while (!readPage.GetFirst(&fetchme)) {
+        if(++currentPage >=file.GetLength()-1) 
+            return 0;
+        file.GetPage(&readPage, currentPage);
+    }
+    return 1;
+}
+
+int VirtualDBFile::GetNext(Record &fetchme, CNF &cnf, Record &literal){
+    ComparisonEngine comp;
+    while(GetNext(fetchme))
+    {
+        if(comp.Compare(&fetchme, &literal, &cnf)) 
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void VirtualDBFile::Load (Schema &myschema, const char *loadpath){
+    FILE * fileLoad = fopen(loadpath,"r");
+    if(!fileLoad)
+    {
+        #ifdef F_DEBUG
+            std::cout<<"File could not be opened";
+            exit(1);
+        #endif
+    }
+    Record pull;
+    while(pull.SuckNextRecord(&myschema,fileLoad))
+    {
+        Add(pull);
+    }
+}
---
+++ P1/VirtualDBFile.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "TwoWayList.h"
+#include "Record.h"
+#include "Schema.h"
+#include "File.h"
+#include "Comparison.h"
+#include "ComparisonEngine.h"
+#include "MetaStruct.h"
+
+class VirtualDBFile{
+    protected:
+    File file;
+	Page writePage;
+	Page readPage;
+	int currentPage;
+    void check_write(){
+    	if(!writePage.empty())
+		{
+			int pos = !file.GetLength()? 0 : file.GetLength()-1;
+			file.AddPage(&writePage, pos);
+			writePage.EmptyItOut();       
+		}
+	}
+    public:
+    VirtualDBFile(){
+        currentPage=0;
+    }
+    friend class DBFile;
+    virtual void Add (Record &addme){};
+    int Create (const char *fpath, fType file_type, void *startup);
+	int Close ();
+	void Load (Schema &myschema, const char *loadpath);
+	void MoveFirst ();
+	int GetNext (Record &fetchme);
+	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
+};
